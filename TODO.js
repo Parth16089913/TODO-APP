c11104e4e40fce4d74833ff8df59b7cb2f245048
Parth Gupta <parthgupta160810@gmail.com>
2024-05-19 16:18:33 +0530

    did a lot of changes

--- TODO.js
+++ TODO.js
@@ -1,13 +1,38 @@
 let input = document.querySelector("input")
 let addBtn = document.querySelector(".button-add")
 let delBtn = document.querySelector(".button-delete-all")
+let ul = document.querySelector("ul")
+addBtn.addEventListener("click",function (){
+    const liNumber = document.querySelectorAll("ul div").length;
 
-addBtn.addEventListener("click", ()=> {
+    if (liNumber < 10) {
+        if (input.value === "") {
+            console.log("nothing typed")
+            return
+        }if (input.value === " "){
+            console.log("space typed")
+            return
+        }
+        else {
+        
+        let chkDiv = document.createElement("div")
+        chkDiv.className = "task-item"
+        let chkInput = document.createElement("input")
+        chkInput.type = "checkbox"
+        chkInput.id = input.value
+        let chkLabel = document.createElement("label")
+        chkLabel.htmlFor = input.value;
+        chkLabel.innerText = input.value
+        ul.appendChild(chkDiv)
+        chkDiv.appendChild(chkInput)
+        chkDiv.appendChild(chkLabel)
+        input.value = ""
+    }
+    } else {
+        alert("Maximum Items Limit reached")
+        input.value= ""
+    }
+    
  
-    let liItem = document.createElement("input", "label")
-    console.log(liItem)
-    input.value = ""
-
 })
-
-
+ 
