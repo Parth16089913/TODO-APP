4282a5d00c519fe75d6678ff5dccdc573a536d5c
Parth Gupta <parthgupta160810@gmail.com>
2024-05-19 14:31:36 +0530

    added

---
+++ TODO.js
@@ -0,0 +1,13 @@
+let input = document.querySelector("input")
+let addBtn = document.querySelector(".button-add")
+let delBtn = document.querySelector(".button-delete-all")
+
+addBtn.addEventListener("click", ()=> {
+ 
+    let liItem = document.createElement("input", "label")
+    console.log(liItem)
+    input.value = ""
+
+})
+
+
